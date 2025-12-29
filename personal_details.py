c67a6a48cfea5c9017c26d7a24a07e74dc0d2f04
NileshBoppana <nileshboppana2008@gmail.com>
2025-12-29 12:22:12 +0530

    Create personal_details.py

---
+++ personal_details.py
@@ -0,0 +1,12 @@
+# Creating variables of different data types
+age = 16                 # integer
+height = 5.8             # float
+name = "Nilesh"          # string
+are_you_student = True   # boolean
+
+# Printing with nice formatting
+print("---- Personal Details ----")
+print(f"Name           : {name}")
+print(f"Age            : {age} years")
+print(f"Height         : {height} feet")
+print(f"Are you student: {are_you_student}")
