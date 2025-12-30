07b3ef2ee00d2fa7f9870409282975283d359344
NileshBoppana <nileshboppana2008@gmail.com>
2025-12-30 18:52:37 +0530

    Create number_guessing_game.py

---
+++ number_guessing_game.py
@@ -0,0 +1,12 @@
+secret_number = 7
+guess = 0
+
+while guess != secret_number:
+    guess = int(input("Guess the number: "))
+
+    if guess > secret_number:
+        print("Too high")
+    elif guess < secret_number:
+        print("Too low")
+
+print("Correct! You guessed the number.")
